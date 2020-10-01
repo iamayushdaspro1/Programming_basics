9500958c83b6a0bc1987d8de847f9eb5308d8eb8
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-10-02 06:12:36 +0700

    Print Hello World using constructor

---
+++ PrintHelloWorld/HelloWorldConstructor.java
@@ -0,0 +1,9 @@
+public class HelloWorldConstructor {
+    HelloWorldConstructor() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorldConstructor();
+    }
+}
