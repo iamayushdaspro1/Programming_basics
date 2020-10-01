a25881a582160a1d400038b7fd786d851d68294d
PRABHU12345384 <72146816+PRABHU12345384@users.noreply.github.com>
2020-10-01 11:57:12 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -4,6 +4,6 @@ public class Main{string[args]
         System.out.println("Hello World");
         //second mehtod of printing
         System.console.readline()
-        //both these programs with the following syntax will produce the same output helo world.
+        //These two programs with the following syntax will produce the same output helo world.
 }
 }
