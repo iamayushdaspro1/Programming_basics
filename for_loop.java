126bced8af7a4e9a55260bd956718e25ceca43fd
Suryasa Made <suryasa.made@gmail.com>
2020-10-01 13:38:06 +0800

    for loop example

---
+++ for_loop.java
@@ -0,0 +1,9 @@
+//use for loop if the number of iteration is fixed
+public class forLoop{
+	public static void main(String[] args){
+	//code of for loop here
+		for(int i=1; i<=10; i++){
+			System.out.println(i);
+		}
+	}
+}
