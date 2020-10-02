b495eb4ee6216e1eb69b9cebc1fb8a4f411a0984
Suryasa Made <suryasa.made@gmail.com>
2020-10-02 15:30:05 +0800

    do-while loop example

---
+++ doWhile_loop.java
@@ -0,0 +1,11 @@
+// use do-while loop if the number of iteration is not fixed and it must have execute the loop at least once
+public class doWhileLoop{
+    public static void main(String[] args){
+        // code of do-while loop here
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=1);
+    }
+}
